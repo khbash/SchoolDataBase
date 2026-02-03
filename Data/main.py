118725da887ce0ad7e922d075a6390ade3a67ff0
Khrystyna Bash <khrystynabash09@gmail.com>
2026-02-03 11:28:06 +0200

    Second step (reading file)

--- Data/main.py
+++ Data/main.py
@@ -3,6 +3,11 @@ import csv
 with open("Data/config.txt", "r", encoding="utf-8") as file:
     min_score = int(file.read().strip())
 
+with open("Data/students.csv", "r", encoding="utf-8") as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            row["score"] = int(row["score"])
+
 
 
 
