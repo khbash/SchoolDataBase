ea243fcab3ba2e2cd53259ebf4d1932f096d3306
Khrystyna Bash <khrystynabash09@gmail.com>
2026-02-03 14:54:49 +0200

    Done whole assignment

--- Data/main.py
+++ Data/main.py
@@ -1,4 +1,5 @@
 import csv
+import json
 
 with open("Data/config.txt", "r", encoding="utf-8") as file:
     min_score = int(file.read())
@@ -15,6 +16,19 @@ with open("Data/retest.csv", "w", encoding="utf-8") as  file:
     writer = csv.DictWriter(file, fieldnames=["id", "name", "surname", "score"])
     writer.writeheader()
     writer.writerows(retest_list)
+
+
+success_list = []
+with open("Data/students.csv", "r", encoding="utf-8") as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            row["score"] = int(row["score"])
+            if row["score"] >= min_score:
+                row["passed"] = True
+                success_list.append(row)
+
+with open("Data/best_students.json", "w", encoding="utf-8") as file:
+    json.dump(success_list, file, ensure_ascii=False, indent=4)
 
 
 
